9c09a6c1bce898239592a520d716947c4b1bad4a
LucasHogent1 <lucas.katamadze@student.hogent.be>
2024-03-19 18:08:48 +0100

    mergefix test

--- src/Componenten/useThemaNavbar.jsx
+++
@@ -1,9 +0,0 @@
-import { useColorModeValue } from "@chakra-ui/react";
-
-export const useNavbarStyles = () => {
-  const bgColor = useColorModeValue("gray.600", "gray.600");
-  const color = useColorModeValue("white", "white");
-  const hoverColor = useColorModeValue("blue.400", "purple.300");
-
-  return { bgColor, color, hoverColor };
-};
